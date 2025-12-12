10bdd3f5e23ad2a30fd6327de5908b68736a89cc
Rafael <rafaelrobertc@gmail.com>
2025-12-11 21:18:15 -0300

    Wrappers 11/12

---
+++ src/Pwrapper/test/test01.java
@@ -0,0 +1,30 @@
+package Pwrapper.test;
+
+import java.util.ArrayList;
+
+public class test01 {
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 20;
+        int i = 30;
+        long l = 40L;
+        float f = 50.0f;
+        double d = 60.0;
+        char c = 'A';
+        boolean bool = true;
+
+        Byte B = 126;
+        Short S = 20;
+        Integer I = 30;
+        Long L = 40L;
+        Float F = 50.0f;
+        Double D = 60.0;
+        Character C = 'A';
+        Boolean Bool = true;
+
+        ArrayList<Integer> Arraylist = new ArrayList<>();
+
+
+
+    }
+}
